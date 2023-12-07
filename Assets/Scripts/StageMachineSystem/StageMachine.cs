a4b2401679d77cd1e8fc5788d36c647567c342a8
Naioku <adrian.komuda.dev@gmail.com>
2023-12-07 21:31:47 +0100

    Removed unnecessary code.

--- Assets/Scripts/StageMachineSystem/StageMachine.cs
+++ Assets/Scripts/StageMachineSystem/StageMachine.cs
@@ -7,21 +7,10 @@ namespace StageMachineSystem
 {
     public class StageMachine
     {
-        private readonly CoroutineManager.CoroutineCaller coroutineCaller;
+        private readonly CoroutineManager.CoroutineCaller coroutineCaller = AllManagers.Instance.CoroutineManager.GenerateCoroutineCaller();
         private Guid tickCoroutineId;
         private BaseStage currentStage;
         private SharedData SharedData { get; set; } = new SharedData();
-
-        public StageMachine()
-        {
-            coroutineCaller = AllManagers.Instance.CoroutineManager.GenerateCoroutineCaller();
-        }
-        
-        public StageMachine(BaseStage initialStage)
-        {
-            coroutineCaller = AllManagers.Instance.CoroutineManager.GenerateCoroutineCaller();
-            SetStage(initialStage);
-        }
 
         public void SetStage(BaseStage newStage)
         {
